e3418c7f1e4391c52edf68aac344df119e578023
manishpun08 <manishpunmagar6@gmail.com>
2025-04-20 12:07:23 +0545

    Add console log statements to page and Header components for debugging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 const page = () => {
+  console.log("test");
+
   return (
     <h1 className="text-center text-3xl">
       Welcome to Electricity Commission FE
--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Header = () => {
+  console.log("hello");
   return <div>Header</div>;
 };
 
