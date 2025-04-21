811d0e889e6fd166f3a28c5a4cb5959b8f7f1ae5
Gaurav-kushwaha20 <rockykushwaha5295@gmail.com>
2025-04-21 17:00:22 +0545

    function duties and authority section completed

---
+++ src/app/about/partials/FunctionDuties.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const FunctionDuties = () => {
+   return (
+      <div className='bg-background-400 py-10'>
+         <p className='typography-h3-bold text-text-500'>Function, Duties and Authorities</p>
+
+         {/* Function */}
+         <div className='mt-5'>
+            <p className='typography-p-regular-medium text-text-600'>1. Function</p>
+            <ul className='mt-5 flex flex-col gap-3'>
+               <li className='typography-p1-regular text-text-400 list-disc list-inside'>Tariff Regulation: Determine electricity tariffs and charges for generation, transmission, distribution, and supply of electricity.</li>
+               <li className='typography-p1-regular text-text-400 list-disc list-inside'>Tariff Regulation: Determine electricity tariffs and charges for generation, transmission, distribution, and supply of electricity.</li>
+               <li className='typography-p1-regular text-text-400 list-disc list-inside'>Tariff Regulation: Determine electricity tariffs and charges for generation, transmission, distribution, and supply of electricity.</li>
+               <li className='typography-p1-regular text-text-400 list-disc list-inside'>Tariff Regulation: Determine electricity tariffs and charges for generation, transmission, distribution, and supply of electricity.</li>
+            </ul>
+         </div>
+
+         {/* Duties */}
+         <div className='mt-9'>
+            <p className='typography-p-regular-medium text-text-600'>2. Duties</p>
+            <ul className='mt-5 flex flex-col gap-3'>
+               <li className='typography-p1-regular text-text-400 list-disc list-inside'>Tariff Regulation: Determine electricity tariffs and charges for generation, transmission, distribution, and supply of electricity.</li>
+               <li className='typography-p1-regular text-text-400 list-disc list-inside'>Tariff Regulation: Determine electricity tariffs and charges for generation, transmission, distribution, and supply of electricity.</li>
+               <li className='typography-p1-regular text-text-400 list-disc list-inside'>Tariff Regulation: Determine electricity tariffs and charges for generation, transmission, distribution, and supply of electricity.</li>
+               <li className='typography-p1-regular text-text-400 list-disc list-inside'>Tariff Regulation: Determine electricity tariffs and charges for generation, transmission, distribution, and supply of electricity.</li>
+            </ul>
+         </div>
+
+         {/* Authority */}
+         <div className='mt-9'>
+            <p className='typography-p-regular-medium text-text-600'>3. Authority</p>
+            <ul className='mt-5 flex flex-col gap-3'>
+               <li className='typography-p1-regular text-text-400 list-disc list-inside'>Tariff Regulation: Determine electricity tariffs and charges for generation, transmission, distribution, and supply of electricity.</li>
+               <li className='typography-p1-regular text-text-400 list-disc list-inside'>Tariff Regulation: Determine electricity tariffs and charges for generation, transmission, distribution, and supply of electricity.</li>
+               <li className='typography-p1-regular text-text-400 list-disc list-inside'>Tariff Regulation: Determine electricity tariffs and charges for generation, transmission, distribution, and supply of electricity.</li>
+               <li className='typography-p1-regular text-text-400 list-disc list-inside'>Tariff Regulation: Determine electricity tariffs and charges for generation, transmission, distribution, and supply of electricity.</li>
+            </ul>
+         </div>
+
+
+
+
+      </div>
+   )
+}
+
+export default FunctionDuties
