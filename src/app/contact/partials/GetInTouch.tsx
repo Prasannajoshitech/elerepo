3664ea972095bbd8d4bdc5e39bed97ca5e109bbe
Gaurav-kushwaha20 <rockykushwaha5295@gmail.com>
2025-04-21 15:37:50 +0545

    contact us page responsive design

--- src/app/contact/partials/GetInTouch.tsx
+++ src/app/contact/partials/GetInTouch.tsx
@@ -1,25 +1,28 @@
-import React from 'react'
-import GetInTouchForm from './GetInTouchForm'
-import ContactUs from './ContactUs'
+import React from "react";
+import GetInTouchForm from "./GetInTouchForm";
+import ContactUs from "./ContactUs";
 
 const GetInTouch = () => {
-   return (
-      <div>
-         <p className='typography-h3-bold text-black'>Get In Touch</p>
-         <p className='mt-3'>Connect with us for services, support, and information related to electricity regulation.</p>
+  return (
+    <div>
+      <p className="typography-h3-bold text-black">Get In Touch</p>
+      <p className="mt-3">
+        Connect with us for services, support, and information related to
+        electricity regulation.
+      </p>
 
-         {/* Get In Touch Form */}
-         <div className='mt-10 grid grid-cols-1 lg:grid-cols-5 gap-10'>
-            <div className='lg:col-span-3'>
-               <GetInTouchForm />
-            </div>
+      {/* Get In Touch Form */}
+      <div className="mt-10 grid grid-cols-1 lg:grid-cols-5 gap-10">
+        <div className="lg:col-span-3">
+          <GetInTouchForm />
+        </div>
 
-            <div className='lg:col-span-2'>
-               <ContactUs />
-            </div>
-         </div>
+        <div className="lg:col-span-2">
+          <ContactUs />
+        </div>
       </div>
-   )
-}
+    </div>
+  );
+};
 
-export default GetInTouch
+export default GetInTouch;
--- src/components/InputTextArea.tsx
+++ src/components/InputTextArea.tsx
@@ -1,46 +1,50 @@
 // components/InputTextarea.tsx
-import React from 'react';
+import React from "react";
 
 interface InputTextareaProps {
-   label: string;
-   name: string;
-   value: string;
-   placeholder: string;
-   error?: string;
-   touched?: boolean;
-   onChange: React.ChangeEventHandler<HTMLTextAreaElement>;
-   onBlur: React.FocusEventHandler<HTMLTextAreaElement>;
+  label: string;
+  name: string;
+  value: string;
+  placeholder: string;
+  error?: string;
+  touched?: boolean;
+  onChange: React.ChangeEventHandler<HTMLTextAreaElement>;
+  onBlur: React.FocusEventHandler<HTMLTextAreaElement>;
 }
 
 const InputTextarea: React.FC<InputTextareaProps> = ({
-   placeholder,
-   label,
-   name,
-   value,
-   error,
-   touched,
-   onChange,
-   onBlur,
+  placeholder,
+  label,
+  name,
+  value,
+  error,
+  touched,
+  onChange,
+  onBlur,
 }) => {
-   return (
-      <div>
-         <label className="block typography-p-regular-medium text-text-400" htmlFor={name}>
-            {label}
-         </label>
-         <textarea
-            name={name}
-            id={name}
-            rows={4}
-            value={value}
-            onChange={onChange}
-            placeholder={placeholder}
-            onBlur={onBlur}
-            className={`w-full mt-3.5 p-5 rounded-lg typography-p-regular text-text-200 border resize-none ${touched && error ? 'border-red-500' : 'border-gray-300'
-               }`}
-         />
-         {touched && error && <p className="text-red-500 text-sm mt-1">{error}</p>}
-      </div>
-   );
+  return (
+    <div>
+      <label
+        className="block typography-p-regular-medium text-text-400"
+        htmlFor={name}
+      >
+        {label}
+      </label>
+      <textarea
+        name={name}
+        id={name}
+        rows={4}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        onBlur={onBlur}
+        className={`w-full mt-3.5 p-5 rounded-lg typography-p-regular text-text-200 border resize-none ${
+          touched && error ? "border-red-500" : "border-gray-300"
+        }`}
+      />
+      {touched && error && <p className="text-red-500 text-sm mt-1">{error}</p>}
+    </div>
+  );
 };
 
 export default InputTextarea;
