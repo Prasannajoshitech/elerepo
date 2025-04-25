c0366e441e1fd70a412e2a320665092896aebe76
manishpun08 <manishpunmagar6@gmail.com>
2025-04-25 12:35:43 +0545

    feat: add social media sharing functionality with modal and update dependencies

--- src/app/act/[slug]/partials/Header.tsx
+++ src/app/act/[slug]/partials/Header.tsx
@@ -1,18 +1,32 @@
-import Image from "next/image";
-import React from "react";
+"use client";
 import facebookIcon from "@/assets/actRuleDetail/facebook.svg";
-import messangerIcon from "@/assets/actRuleDetail/messanger.svg";
-import xIcon from "@/assets/actRuleDetail/x.svg";
 import mailIcon from "@/assets/actRuleDetail/mail.svg";
+import messangerIcon from "@/assets/actRuleDetail/messanger.svg";
 import shareIcon from "@/assets/actRuleDetail/share.svg";
-import { IActDetailData } from "../interface/actDetail.interface";
+import xIcon from "@/assets/actRuleDetail/x.svg";
+import SocialMediaShareModal from "@/components/modal/SocialMediaShareModal";
 import { formatToNepaliDate } from "@/utils/formatToNepaliDate";
+import Image from "next/image";
+
+import React, { useState } from "react";
+import {
+  EmailShareButton,
+  FacebookMessengerShareButton,
+  FacebookShareButton,
+  TwitterShareButton,
+} from "react-share";
+import { IActDetailData } from "../interface/actDetail.interface";
 
 interface Props {
   actDetailData: IActDetailData;
 }
 
 const Header: React.FC<Props> = ({ actDetailData }) => {
+  const [openModal, setOpenModal] = useState(false);
+  const [shareLink, setShareLink] = useState("");
+
+  const currentUrl = typeof window !== "undefined" ? window.location.href : "";
+
   return (
     <div className="mt-10 padding-x ">
       <div className="flex items-center justify-between">
@@ -23,42 +37,57 @@ const Header: React.FC<Props> = ({ actDetailData }) => {
           </p>
         </div>
 
-        {/* Icons */}
+        {/* Share Icons  */}
         <div className="flex items-center gap-3">
-          <Image
-            alt="facebook"
-            src={facebookIcon}
-            width={24}
-            height={24}
-            className="w-6 h-6 object-cover object-center"
-          />
-          <Image
-            alt="messangerIcon"
-            src={messangerIcon}
-            width={24}
-            height={24}
-            className="w-6 h-6 object-cover object-center"
-          />
-          <Image
-            alt="xIcon"
-            src={xIcon}
-            width={24}
-            height={24}
-            className="w-6 h-6 object-cover object-center"
-          />
-          <Image
-            alt="mailIcon"
-            src={mailIcon}
-            width={24}
-            height={24}
-            className="w-6 h-6 object-cover object-center"
-          />
+          <FacebookShareButton url={currentUrl}>
+            <Image
+              alt="facebook"
+              src={facebookIcon}
+              width={24}
+              height={24}
+              className="w-6 h-6 object-cover object-center"
+            />
+          </FacebookShareButton>
+
+          <FacebookMessengerShareButton appId="Messenger" url={currentUrl}>
+            <Image
+              alt="messangerIcon"
+              src={messangerIcon}
+              width={24}
+              height={24}
+              className="w-6 h-6 object-cover object-center"
+            />
+          </FacebookMessengerShareButton>
+
+          <TwitterShareButton url={currentUrl}>
+            <Image
+              alt="xIcon"
+              src={xIcon}
+              width={24}
+              height={24}
+              className="w-6 h-6 object-cover object-center"
+            />
+          </TwitterShareButton>
+          <EmailShareButton url={currentUrl}>
+            <Image
+              alt="mailIcon"
+              src={mailIcon}
+              width={24}
+              height={24}
+              className="w-6 h-6 object-cover object-center"
+            />
+          </EmailShareButton>
+
           <Image
             alt="shareIcon"
             src={shareIcon}
             width={24}
             height={24}
-            className="w-6 h-6 object-cover object-center"
+            className="w-6 h-6 object-cover object-center cursor-pointer"
+            onClick={() => {
+              setOpenModal(true);
+              setShareLink(currentUrl);
+            }}
           />
         </div>
       </div>
@@ -68,6 +97,12 @@ const Header: React.FC<Props> = ({ actDetailData }) => {
           Published Date : {formatToNepaliDate(actDetailData?.created_at)}
         </p>
       </div>
+
+      <SocialMediaShareModal
+        isOpen={openModal}
+        onClose={() => setOpenModal(false)}
+        shareLink={shareLink}
+      />
     </div>
   );
 };
