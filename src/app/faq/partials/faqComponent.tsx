acae09846b3ece88d1e744a96f61b0dec07e778a
manishpun08 <manishpunmagar6@gmail.com>
2025-06-05 15:39:50 +0545

    fix: rename IFaqDaum to IFaqData for consistency in FAQ interfaces

--- src/app/faq/partials/faqComponent.tsx
+++ src/app/faq/partials/faqComponent.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { ChevronDown } from "lucide-react";
 import { useState } from "react";
-import { IFaqDaum } from "../interface/faq.interface";
+import { IFaqData } from "../interface/faq.interface";
 
 interface AccordionProps {
-  items: IFaqDaum[];
+  items: IFaqData[];
 }
 
 export default function Accordion({ items }: AccordionProps) {
